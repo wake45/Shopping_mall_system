732b279856aceecf5a051754e4b5ebab4d9d08a0
LeeSangUk <wake45@naver.con>
2025-01-21 15:15:46 +0900

    first commit

---
+++ src/main/kotlin/kotlinproject/shoppingmall/HelloController.kt
@@ -0,0 +1,12 @@
+package kotlinproject.shoppingmall
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+    @GetMapping("/")
+    fun hello(): String {
+        return "Hello, World!"
+    }
+}
---
+++ src/main/kotlin/kotlinproject/shoppingmall/ShoppingmallApplication.kt
@@ -0,0 +1,11 @@
+package kotlinproject.shoppingmall
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShoppingmallApplication
+
+fun main(args: Array<String>) {
+	runApplication<ShoppingmallApplication>(*args)
+}
